4da2240631dd6c3e63ad5457912b96a0083dbbf4
Alfi-Shahrin-Tamim <bsse1816@iit.du.ac.bd>
2026-07-26 20:02:00 +0600

    Update exchange-sort.c

--- exchange-sort.c
+++ exchange-sort.c
@@ -3,7 +3,7 @@
 int main(){
     int arr[5] = {3, 4, 6 ,1, -324};
     for(int i = 0 ; i< 5 - 1; i++){
-        for(int j = i + 1; j < 5  ;j++){
+        for(int j = i + 1; j < 5 - 1 ;j++){
             if(arr[i] > arr[j]){
                 int temp = arr[i];
                 arr[i] = arr[j];
